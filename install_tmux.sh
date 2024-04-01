df7b6d3bfa685250f67efd194adbf3866497e350
roneydua <roneyddasilva@gmail.com>
2024-04-01 09:55:20 -0300

    new tmux configs

--- install_tmux.sh
+++ install_tmux.sh
@@ -1,14 +1,33 @@
 #! /bin/bash
+# Inspirated on https://dev.to/andrenbrandao/terminal-setup-with-zsh-tmux-dracula-theme-48lm
 
-echo "copiando os arquivos de configuracao tmux"
-git clone https://github.com/gpakosz/.tmux.git
-ln -s -f .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local .
 
-#git clone https://github.com/gpakosz/.tmux.git /path/to/oh-my-tmux
-#ln -s -f /path/to/oh-my-tmux/.tmux.conf ~/.tmux.conf
-#cp /path/to/oh-my-tmux/.tmux.conf.local ~/.tmux.conf.local
+echo "copiando os arquivos de configuracao tmux and zsh files"
+sudo apt install guake zsh tmux
 
-cp ./tmuxConfiguracao/.tmux.conf ~/
+chsh -s $(which zsh)
+echo "In Guake go to Preferences → Shell and set your default interpreter to /usr/bin/zsh. Restart it and you should see the message below."
+echo "Installing Oh My Zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cd ~/.oh-my-zsh/custom/plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+echo "Restart your Zsh with source ~/.zshrc."
+
+echo "Configure Guake by opening Preferences → Apperance. Then, uncheck "Use the system fixed width font" and select MesloLGS NF Regular. For more details check their guide here."
+
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+
+
+
+
+cp ./tmux_zsh_files/.tmux.conf ./tmux_zsh_files/.zshrc ~/
 
 echo "run ctrl+b shift+i to reload"
