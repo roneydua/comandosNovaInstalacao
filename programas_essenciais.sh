b10b2ac5c60003ce30b8c5bfa987884b564fdaa7
roneydua <roneyddasilva@gmail.com>
2024-05-07 13:32:15 -0300

    add programs

--- programas_essenciais.sh
+++ programas_essenciais.sh
@@ -2,7 +2,7 @@
 
 apt-get install clementine synaptic tmux hplip-gui vlc p7zip pdftk yt-dlp conky system-config-printer dconf-editor git thunderbird birdtray variety xournal qtikz maxima wxmaxima octave jstest-gtk -y
 
-apt-get install okular okular-extra-backends wavemon kolourpaint shotwell inkscape gimp cutecom tree doxygen baobab tesseract-ocr qpdf -y
+apt-get install okular okular-extra-backends wavemon kolourpaint shotwell inkscape gimp cutecom tree doxygen baobab tesseract-ocr qpdf remmina -y
 
  
 
